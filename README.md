cecc7c557602565fcb0d4f1bec8ae8f7e08ea2d5
hatsuroku <hatsuroku@foxmail.com>
2024-01-09 13:40:33 +0800

    moved to python

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # leetcode_template
+
+## 2024-01-09 Update
+
+Moved to python since python is more elegant and convenient.
+
+There are my notes, which contains my thoughts and anylasis.
+
+[做题题目记录 (notion.so)](https://www.notion.so/5e1b723cca3e4b259dc821bda60496e7?v=ca02fd6b06f2483da640a7d2f2bed3e4)
+
+
+
+
+
+## Previous description
+
 My code template for leetcode
 
 C++ is mainly used.
--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from typing import List
 from functools import cache
 from math import inf
 from typing import List, Optional
+from sortedcontainers import SortedList
+import heapq
+
 
 import sys
+import math
 import time
+import functools
+from bisect import bisect_left, bisect_right
+from collections import deque, defaultdict
 
 
 
@@ -14,29 +21,48 @@ class TreeNode:
         self.left = left
         self.right = right
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        pass
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        dic = set(dictionary)
+        f = [inf for _ in range(len(s) + 1)]
+        # f[i] 是 s[0,i) 分割后剩下的最小字符数
+        f[0] = 0
+        
+        for i in range(1, len(s) + 1):
+            f[i] = f[i - 1] + 1
+            for j in range(i, 0, -1):
+                sub = s[j-1:i]
+                if sub in dic:
+                    f[i] = min(f[i], f[j - 1])
+        
+        return f[-1]
+                
+         
+        
 
 
 if __name__ == '__main__':
-    input_file = open('input.txt', 'r', encoding='utf-8')
-    output_file = open('output.txt', 'w', encoding='utf-8')
-    sys.stdin = input_file
-    sys.stdout = output_file
+    # input_file = open('input.txt', 'r', encoding='utf-8')
+    # output_file = open('output.txt', 'w', encoding='utf-8')
+    # sys.stdin = input_file
+    # sys.stdout = output_file
 
     solu = Solution()
 
     t1 = time.time()
 
-    l = [1 for _ in range(10 ** 7)]
-    print(l[0])
-    for i in range(1, 10 ** 7):
-        l[i] += l[i - 1]
-        print(l[i])
-
+    ret = solu.minDistance("horse", "ros")
+    print(ret)
+    
     t2 = time.time()
-    print(f'use {t2 - t1} seconds')
+    print(t2 - t1)
 
-    input_file.close()
-    output_file.close()
+
+    # input_file.close()
+    # output_file.close()
